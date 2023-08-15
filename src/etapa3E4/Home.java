f5d533388de255d6b43e7c0a35f3b521ef358e66
Ben <100096813+BenRomeroArg@users.noreply.github.com>
2023-08-15 16:44:32 -0300

    vista de ingresar producto

--- src/etapa3E4/Home.java
+++ src/etapa3E4/Home.java
@@ -58,6 +58,11 @@ public class Home extends javax.swing.JFrame {
         jMenu1.setText("Administracion");
 
         jMenuItem1.setText("Productos");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
         jMenu1.add(jMenuItem1);
 
         jMenuBar1.add(jMenu1);
@@ -112,6 +117,16 @@ public class Home extends javax.swing.JFrame {
         escritorio.add(bn);
         escritorio.moveToFront(bn);
     }//GEN-LAST:event_jMenuNombreActionPerformed
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        // TODO add your handling code here:
+         escritorio.removeAll();
+        escritorio.repaint();
+        crearProducto bn = new crearProducto();
+        bn.setVisible(true);
+        escritorio.add(bn);
+        escritorio.moveToFront(bn);
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
